82188acda237caf2d535b825ba7ee28ad132f11e
DESKTOP-ODKQHGA\cg <yoman2251@gmail.com>
2018-11-07 14:00:29 +0800

    Monte Carlo (pthread)

--- Monte Carlo (pthread)/src/main.c
+++ Monte Carlo (pthread)/src/main.c
@@ -8,7 +8,7 @@
 #include <math.h>
 #include <stdlib.h>
 
-	
+
 int amtWanted = 0;
 int totalPts  = 0;
 
@@ -20,17 +20,17 @@ int main()
     pthread_t thread;
 
     do
-	{
+		{
         printf("Please enter a positive number for the amount of points you would like to generate? \n");
-        scanf("%d", &amtWanted);  	
-	}while (amtWanted <= 0);
-  
+        scanf("%d", &amtWanted);
+		}while (amtWanted <= 0);
+
     pthread_create(&thread, NULL, &count, NULL);
     pthread_join(thread, NULL);
-    
+
     double pi = 4.0 * totalPts / amtWanted;
     printf("The approximate value of pi for the desired amount of points (%d) is: %f \n", amtWanted, pi);
-    printf("\n");  
+    printf("\n");
     return 0;
 }
 
@@ -40,10 +40,10 @@ void *count(void *X)
 
     /* initialize random numbers */
     for (i=0; i < amtWanted; i++)
-    {	
+    {
         double x = (double)rand() / RAND_MAX;
         double y = (double)rand() / RAND_MAX;
-        
+
         if (((x * x) + (y * y)) < 1)
         {
             totalPts++;
