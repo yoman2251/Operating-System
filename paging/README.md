78d89f8aabe7f39e6ba19820ff3e340e1a4ef4d5
DESKTOP-ODKQHGA\cg <yoman2251@gmail.com>
2018-12-05 10:46:03 +0800

    paging

---
+++ paging/README.md
@@ -0,0 +1,15 @@
+Programming Problems<br>
+====
+Chap.8 8.25*:<br>
+------ 
+Assume that a system has a 32-bit virtual address with a 4-KB page size.<br>
+Write a C program that is passed a virtual address (in decimal) on the command line and have it output the page number and offset for the given address.<br>
+As an example, your program would run as follows:<br>
+* ./a.out 19986
+Your program would output:<br>
+The address 19986 contains:<br>
+* page number=4
+* offset=3602
+<br>
+Writing this program will require using the appropriate data type to store 32 bits.<br>
+We encourage you to use unsigned data types as well.<br>
--- paging/main.c
+++ paging/main.c
@@ -6,22 +6,21 @@
 
 int main(int argc, char *argv[])
 {
-unsigned long page;
-unsigned long offset;
-unsigned long address;
+    unsigned long page;
+    unsigned long offset;
+    unsigned long address;
 
 
-address= atoll(argv[1]);
-/* Page Number =  quotient of  address / 4KB and offset = remainder*/
-/*Below is the faster method of calculating the same*/
+    address= atoll(argv[1]);
+    /* Page Number =  quotient of  address / 4KB and offset = remainder*/
+    /*Below is the faster method of calculating the same*/
 
-page = address >> 12; /* Since page size is 4KB => 12 bits holding the virtual address*/
-offset = address & 0xfff;
+    page = address >> 12; /* Since page size is 4KB => 12 bits holding the virtual address*/
+    offset = address & 0xfff;
 
-printf("The address %lu contains: \n", address);
-printf("page number = %lu\n",page);
-printf("offset = %lu\n", offset);
+    printf("The address %lu contains: \n", address);
+    printf("page number = %lu\n",page);
+    printf("offset = %lu\n", offset);
 
-
-return 0;
+    return 0;
 }
